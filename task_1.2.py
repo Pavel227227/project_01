5217a268c754a50e25541a76ae2925f2be832e5c
Pavel227227 <60576511+Pavel227227@users.noreply.github.com>
2023-06-16 16:39:40 +0300

    Add files via upload
    Задача 1.2

---
+++ task_1.2.py
@@ -0,0 +1,35 @@
+
+# import random
+# my_favorite_songs = [
+#     ['Waste a Moment', 3.03],
+#     ['New Salvation', 4.02],
+#     ['Staying\' Alive', 3.40],
+#     ['Out of Touch', 3.03],
+#     ['A Sorta Fairytale', 5.28],
+#     ['Easy', 4.15],
+#     ['Beautiful Day', 4.04],
+#     ['Nowhere to Run', 2.58],
+#     ['In This World', 4.02],
+# ]
+# random_songs = random.sample(my_favorite_songs, 3)
+# total_time = sum([song[1] for song in random_songs])
+
+# print(f"Суммарное время звучания трёх песен {total_time} минут.")
+import random
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+random_values = random.sample(list(my_favorite_songs_dict.values()), 3)
+
+total_time_dict = sum(s for s in random_values)
+print(f"Три песни звучат {total_time_dict} минут")
+
